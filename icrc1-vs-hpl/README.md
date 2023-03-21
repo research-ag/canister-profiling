4c79b372a56851c8d06dc0acb2ea235e7358e56e
andy <andy.gura.jd@gmail.com>
2023-03-21 12:28:48 +0200

    fixed icrc1-hpl comparison script for mac

--- icrc1-vs-hpl/README.md
+++ icrc1-vs-hpl/README.md
@@ -1,8 +1,10 @@
 This script makes a cycles consumption comparison between ICRC1 ledger and HPL.
 
 How to run script:
+1) ensure that `dfx` is running
+1) ensure that `ic-repl` installed and available in `$PATH`
 1) `bash profiler.sh`
-2) Observe updated `stats.md`
+1) Observe updated `stats.md`
 
 Using with custom MOC path:
 `DFX_MOC_PATH=/home/andy/bin/moc bash profiler.sh`
--- icrc1-vs-hpl/profiler.sh
+++ icrc1-vs-hpl/profiler.sh
@@ -7,7 +7,7 @@ if [ ! -e "$TEMPD" ]; then
     exit 1
 fi
 trap 'rm -rf "$TEMPD"; exit 0' EXIT
-trap 'rm -rf "$TEMPD"; exit 2' HUP INT QUIT TERM STOP PWR
+trap 'rm -rf "$TEMPD"; exit 2' HUP INT QUIT TERM STOP
 
 echo working in ${TEMPD}
 cp test_ledger.repl ${TEMPD}
@@ -18,7 +18,7 @@ pushd ${TEMPD}
 git clone git@github.com:research-ag/hpl.git
 pushd hpl/test
 sed -i -E 's/"args": "/"args": "--force-gc /' dfx.json
-echo -e "$(cat ../../hpl_patch_imports.mo)\n$(head -n -1 test_api/ledger_test_api.mo)\n$(cat ../../hpl_patch_functions.mo)\n};\n" > test_api/ledger_test_api.mo
+echo -e "$(cat ../../hpl_patch_imports.mo)\n$(sed '$d' test_api/ledger_test_api.mo)\n$(cat ../../hpl_patch_functions.mo)\n};\n" > test_api/ledger_test_api.mo
 dfx build --check
 popd
 
@@ -26,7 +26,7 @@ popd
 git clone https://github.com/NatLabs/icrc1.git
 pushd icrc1
 sed -i -E 's/"args": "/"args": "--force-gc /' dfx.json
-echo -e "$(cat ../icrc1_patch_imports.mo)\n$(head -n -1 src/ICRC1/Canisters/Token.mo)\n$(cat ../icrc1_patch_functions.mo)\n};\n" > src/ICRC1/Canisters/Token.mo
+echo -e "$(cat ../icrc1_patch_imports.mo)\n$(sed '$d' src/ICRC1/Canisters/Token.mo)\n$(cat ../icrc1_patch_functions.mo)\n};\n" > src/ICRC1/Canisters/Token.mo
 dfx build --check
 popd
 
